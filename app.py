b0a079cf22acac867745e646d5eaa06fdc6d4e19
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-02-18 17:30:08 -0600

    HW-update

--- app.py
+++ app.py
@@ -5,27 +5,42 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
 
+import datetime
+from datetime import date, timedelta
+
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
+# need all dates
+
+## loop through dates here
+
+# Converting them to a list of strings
+str_dates = []
+for date in date_list:
+    new_date = date.strftime("%Y-%m-%d")
+    str_dates.append(new_date)
 
 #################################################
 # Flask Routes
@@ -36,49 +51,45 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) 
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    """precipitation query"""
+    # Query data
+    results = session.query(Measurement.date).all()
+
+    precipitation = []
+
+    ## precipitation loop here
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    return jsonify(precipitation)
 
-    return jsonify(all_names)
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) 
+     results = session.query(Station)
 
+    """precipitation query"""
+    # Query data
+    results = session.query(Measurement.date).all()
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    station = []
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    ## precipitation loop here
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
+    return jsonify(station)
 
 
 if __name__ == '__main__':
